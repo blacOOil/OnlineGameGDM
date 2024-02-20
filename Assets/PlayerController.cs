e038ac40139ba643a239a8d8aedd388609456e2c
blacOOil <111561598+blacOOil@users.noreply.github.com>
2024-02-21 01:44:39 +0700

    FirstPersonCameraSystem

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,26 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
+
 public class PlayerController : NetworkBehaviour
 {
+    
+    public Camera playerCamera,MainCam;
     public CharacterController Controller;
     public float speed;
+    public float runSpeed;
     public float ObjectiveCollected = 0;
+   
+   
+    public float mouseSensitivity = 100f;
 
+    Vector3 moveDirection = Vector3.zero;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+   
+    float rotationX = 0;
+    public bool canMove = true;
     // Update is called once per frame
+    void Start()
+    {
+        MainCam.gameObject.SetActive(false);
+        playerCamera.gameObject.SetActive(false);
+        // Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.visible = false;
+        runSpeed = speed +10;
+
+        if (IsLocalPlayer)
+        {
+            playerCamera.gameObject.SetActive(true);
+        }
+    }
     void Update()
     {
         if (IsOwner)
-        {
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        { 
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
 
-            if (direction.magnitude >= 0.1f)
-            {
-                Controller.Move(direction * speed * Time.deltaTime);
-            }
-
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            bool isRunning = Input.GetKey(KeyCode.LeftShift);
+            float curSpeedX = canMove ? (isRunning ? runSpeed : speed) * Input.GetAxis("Vertical") : 0;
+            float curSpeedY = canMove ? (isRunning ? runSpeed : speed) * Input.GetAxis("Horizontal") : 0;
+            float movementDirectionY = moveDirection.y;
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+            Controller.Move(moveDirection * Time.deltaTime);
         }
     }
 
@@ -40,4 +70,5 @@ public class PlayerController : NetworkBehaviour
             ObjectiveCollected = +1;
         }
     }
+    
 }
