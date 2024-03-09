a3d4bf27eed85db84ac91f05c5aa258de43cd7ad
blacOOil <111561598+blacOOil@users.noreply.github.com>
2024-03-09 23:37:29 +0700

    Fixed: walking code,Start Boxing

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -52,14 +52,14 @@ public class PlayerController : NetworkBehaviour
     {
         if (IsOwnedByServer || IsClient) // Combined condition for server and client
         {
-            //Debug.Log("Moving...");
+            Debug.Log("Moving...");
            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
 
-            Vector3 forward = transform.TransformDirection(Vector3.forward);
-            Vector3 right = transform.TransformDirection(Vector3.right);
+            Vector3 forward = transform.forward;
+            Vector3 right = transform.right;
 
             bool isRunning = Input.GetKey(KeyCode.LeftShift);
             float curSpeedX = canMove ? (isRunning ? runSpeed : speed) * Input.GetAxis("Vertical") : 0;
@@ -67,7 +67,8 @@ public class PlayerController : NetworkBehaviour
 
             float movementDirectionY = moveDirection.y;
             moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-            Controller.Move(moveDirection * Time.deltaTime);
+            transform.Translate(moveDirection * Time.deltaTime,Space.Self);
+           
         }
     }
 
